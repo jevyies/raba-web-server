90694299ffbe50af84f36e46c808524ed589a2fb
Jevy <jevyababa@gmail.com>
2022-11-25 13:25:45 +0800

    commit changes

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.redirect(`/${uuidV4()}`)
+  res.redirect(`/api/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
+app.get('/api/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
